b68ea1eef02bab4fac2d38821ce188567c450659
Hanjun Kim <hallazzang@gmail.com>
2019-06-27 23:58:36 +0900

    implement add requests page

--- src/pages/addRequests/index.js
+++ src/pages/addRequests/index.js
@@ -1,128 +1,123 @@
 // @format
 
 import React from 'react';
-import {
-  Input,
-  Popconfirm,
-  Button,
-  Table,
-  Descriptions,
-  Select,
-  AutoComplete,
-} from 'antd';
 import axios from 'axios';
+import { Input, Popconfirm, Button, Table, Descriptions, message } from 'antd';
 
-class AddEventPage extends React.Component {
+const apiServer = 'https://code.hanjun.kim:8000';
+
+class AddRequestsPage extends React.Component {
   state = {
-    eventItems: [],
-  };
-
-  handleAcceptButton = id => e => {
-    axios
-      .patch(`https://code.hanjun.kim:8000/addRequests/${id}`, {
-        accepted: true,
-      })
-      .then(() => {
-        axios
-          .get('https://code.hanjun.kim:8000/addRequests?accepted=false')
-          .then(response => response.data)
-          .then(data => {
-            this.setState({ eventItems: data });
-          })
-          .catch(response => {
-            console.log(response);
-          });
-      });
+    requests: [],
   };
 
   columns = [
     {
+      title: 'ID',
+      dataIndex: 'id',
+    },
+    {
       title: '카테고리',
       dataIndex: 'category',
-      key: 'category',
     },
     {
-      title: '혜택명',
+      title: '이름',
       dataIndex: 'name',
-      key: 'name',
     },
     {
-      title: '승인',
-      key: 'action',
-      render: record => (
-        <Popconfirm
-          title="정말로 승인하시겠습니까?"
-          onConfirm={this.handleAcceptButton(record.id)}
-          okText="확인"
-          cancelText="취소"
-        >
-          <Button type="primary">승인하기</Button>
-        </Popconfirm>
+      title: '혜택',
+      dataIndex: 'benefit',
+    },
+    {
+      title: '위치',
+      dataIndex: 'location',
+    },
+    {
+      title: '작업',
+      render: (_, record) => (
+        <span>
+          <Button type="primary" onClick={this.handleAccept(record)}>
+            승인하기
+          </Button>
+        </span>
       ),
     },
   ];
+  expandedRowRender = record => (
+    <Descriptions bordered>
+      <Descriptions.Item label="출처">{record.reference}</Descriptions.Item>
+      <Descriptions.Item label="대상">{record.target}</Descriptions.Item>
+      <Descriptions.Item label="조건">{record.requirements}</Descriptions.Item>
+      <Descriptions.Item label="시작일">{record.beginDate}</Descriptions.Item>
+      <Descriptions.Item label="종료일">{record.endDate}</Descriptions.Item>
+      <Descriptions.Item label="링크">
+        {record.link ? (
+          <a target="_blank" href={record.link}>
+            {record.link}
+          </a>
+        ) : (
+          ''
+        )}
+      </Descriptions.Item>
+      <Descriptions.Item label="이미지">
+        {record.image ? (
+          <a target="_blank" href={record.image}>
+            {record.image}
+          </a>
+        ) : (
+          ''
+        )}
+      </Descriptions.Item>
+      <Descriptions.Item label="전화번호">{record.tel}</Descriptions.Item>
+      <Descriptions.Item label="비고">{record.note}</Descriptions.Item>
+      <Descriptions.Item label="상세">
+        <pre>{record.description}</pre>
+      </Descriptions.Item>
+    </Descriptions>
+  );
 
-  // {
-  //   "id": 1,
-  //   "name": "Name",
-  //   "category": "Category",
-  //   "requirements": "Requirements",
-  //   "begin_date": "Begin Date",
-  //   "end_date": "End Date",
-  //   "reference": "Reference",
-  //   "image": "Image",
-  //   "link": "Link",
-  //   "location": "Location",
-  //   "benefit": "Benefit",
-  //   "target": "Target",
-  //   "tel": "Tel",
-  //   "note": "Note",
-  //   "description": "Description",
-  //   "accepted": false
-  // }
+  fetchRequests = () => {
+    axios
+      .get(`${apiServer}/addRequests?_sort=id`)
+      .then(resp =>
+        this.setState({
+          requests: resp.data.map(item =>
+            Object.assign(item, { key: item.id })
+          ),
+        })
+      )
+      .catch(this.handleError);
+  };
 
-  // <Table dataSource={this.state.eventItems} coulmns={this.columns} />;
+  handleError = err => {
+    if (err.response) {
+      message.error(`에러: ${err.response.status}`);
+    } else if (err.request) {
+      message.error(`에러: ${err.request}`);
+    } else {
+      message.error(`에러: ${err.message}`);
+    }
+  };
 
-  async componentDidMount() {
-    const items = await axios.get(
-      'https://code.hanjun.kim:8000/addRequests?accepted=false'
-    );
-    const { data } = items;
+  handleAccept = record => () => {
+    console.log(record);
+    // TODO: work here
+  };
 
-    this.setState({
-      eventItems: data,
-    });
+  componentDidMount() {
+    this.fetchRequests();
   }
 
   render() {
-    const { eventItems } = this.state;
+    const { requests } = this.state;
     return (
       <Table
-        expandedRowRender={record => (
-          <Descriptions bordered border>
-            <Descriptions.Item label="이름">{record.name} </Descriptions.Item>
-            <Descriptions.Item label="카테고리">
-              {record.category}
-            </Descriptions.Item>
-            <Descriptions.Item label="혜택내역">
-              {record.benefit}
-            </Descriptions.Item>
-            <Descriptions.Item label="대상">{record.target} </Descriptions.Item>
-            <Descriptions.Item label="링크">{record.link} </Descriptions.Item>
-            <Descriptions.Item label="전화번호">{record.tel}</Descriptions.Item>
-            <Descriptions.Item label="승인상태">
-              {record.accepted}
-            </Descriptions.Item>
-            <Descriptions.Item label="상세내역">
-              {record.description}
-            </Descriptions.Item>
-          </Descriptions>
-        )}
-        dataSource={eventItems}
+        expandedRowRender={this.expandedRowRender}
+        dataSource={requests}
         columns={this.columns}
       />
     );
   }
 }
 
-export default AddEventPage;
+export default AddRequestsPage;
